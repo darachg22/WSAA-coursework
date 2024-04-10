9939963639b8224d71eb45d4a05861e1ee3affab
Darach Gorham <darachg22@gmail.com>
2024-04-10 10:13:28 +0100

    First assignment added.

---
+++ assignments/CurrentWeather.py
@@ -0,0 +1,6 @@
+#Using the URL below
+#https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m
+#Write a python program called currentweather.py that will print out the current temperature on the console (and only the temperature)
+#I have set the lat/long to my location, you may use that or a different location.
+
+Url = "https://api.open-meteo.com/v1/forecast?latitude=53.82&longitude=-9.5&current=temperature_2m"
