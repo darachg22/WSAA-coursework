85c50956d99f1a3d663532779a44747e502fa75e
Darach Gorham <darachg22@gmail.com>
2024-04-10 12:27:18 +0100

    assignment 3 completed.

---
+++ assignments/assignment03-cso.py
@@ -0,0 +1,20 @@
+#source used as skeleton: https://stackabuse.com/how-to-get-json-from-a-url-in-python/
+
+import requests
+import json
+
+#Sends a GET request to the URL
+response = requests.get('https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en')
+
+#Parses the response as JSON
+data = response.json()
+#Sends the file to the same folder I keep the code. This would not work until I used the 'r' to convert the line.
+
+file_path = r"C:\Users\darac\OneDrive\Desktop\WSAA-Coursework\assignments\data.json"
+
+#Creates new file
+with open(file_path, "w") as json_file:
+    #Write the JSON data to the file
+    #source used: https://ioflood.com/blog/python-write-json-to-file/ 
+    json.dump(data, json_file)
+
