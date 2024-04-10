f8ddac9e2015512d27a62f746c7a671877f9e8b9
Darach Gorham <darachg22@gmail.com>
2024-04-10 11:36:55 +0100

    Finished code with extra marks section completed.

--- assignments/CurrentWeather.py
+++ assignments/CurrentWeather.py
@@ -21,5 +21,15 @@ data = response.json()
 #Gets the current temperature
 current_temperature = data["current"]["temperature_2m"]
 
-#Prints the current temperature
+#Printing the current temperature
 print(f"Current temperature: {current_temperature} °C")
+
+
+#Last Few Marks
+#This code adds the wind direction to output.
+#I just changed the above code to provide the wind direction
+URL2 = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"
+response2 = requests.get(URL2)
+data = response2.json()
+wind_direction = data["current"]["wind_speed_10m"]
+print(f"Current wind direction: {wind_direction}km/h")
