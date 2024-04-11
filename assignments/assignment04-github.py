817228baf9da423d69ff36b36e76b37a9e8f0381
Darach Gorham <darachg22@gmail.com>
2024-04-11 12:39:15 +0100

    Hopefully fixed commit issues due to the token being in the code.

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -4,13 +4,21 @@
 #I do not need to see your keys (see lab2, to follow)
 #Handup: Push the program as assignment04-github.py to assignments repository.
 #Marks: Marks will be given for the functionality and layout of the code; I do expect minimal comments to indicate you know what the program is doing.
+#token: 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
 
 #Source to read file: https://python-forum.io/thread-26072.html
 
-#from github import Github
+from github import Github
+
+
+g = Github("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")
+
+repo = g.get_repo("darachg22/WSAA-coursework")
+
+file_content = repo.get_contents("assignments/Andrew.txt")
+
+print(file_content)
 
 
 
 
-
-#Push back to Repository
