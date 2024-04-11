94616d51f51cc82573a9d5913a410a718694d75a
Darach Gorham <darachg22@gmail.com>
2024-04-11 10:56:42 +0100

    Would not push the last few tries. This seems to be due to the fact I have my token information in the file.

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -4,23 +4,13 @@
 #I do not need to see your keys (see lab2, to follow)
 #Handup: Push the program as assignment04-github.py to assignments repository.
 #Marks: Marks will be given for the functionality and layout of the code; I do expect minimal comments to indicate you know what the program is doing.
-#token: 
 
 #Source to read file: https://python-forum.io/thread-26072.html
 
 #from github import Github
 
 
-#g = Github(")
 
-#repo = g.get_repo("darachg22/WSAA-coursework")
-
-#file_content = repo.get_contents("assignments/Andrew.txt")
-
-#print(file_content)
-
-
-#Change the letters
 
 
 #Push back to Repository
