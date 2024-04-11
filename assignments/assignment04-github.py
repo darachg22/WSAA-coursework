93435ed5139c8a441f3bc84df0c10fbfdf085b5c
Darach Gorham <darachg22@gmail.com>
2024-04-11 15:43:45 +0100

    Updated assignments 4. Seems to be Ok but will not work so far.

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -4,21 +4,29 @@
 #I do not need to see your keys (see lab2, to follow)
 #Handup: Push the program as assignment04-github.py to assignments repository.
 #Marks: Marks will be given for the functionality and layout of the code; I do expect minimal comments to indicate you know what the program is doing.
-#token: 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
-
+#token: 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
 #Source to read file: https://python-forum.io/thread-26072.html
+
+#from github import Github
+
+#g = Github("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")
+#repo = g.get_repo("darachg22/WSAA-coursework")
+#content = repo.get_contents("assignments/Andrew.txt")
+#repo.update_file(content.path, "more tests", "more tests", content.sha, branch="test")
 
 from github import Github
 
-
-g = Github("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")
-
+#Connect to GitHub
+g = Github("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")
 repo = g.get_repo("darachg22/WSAA-coursework")
 
-file_content = repo.get_contents("assignments/Andrew.txt")
+#source code used: https://github.com/santiagomoneta/pygithub-example/blob/main/main.py
+file = repo.get_contents("Assignments/Andrew.txt", ref="main")
+file_path = file.path
+file_content = (file.decoded_content).decode('utf-8').replace("Andrew", "Darach") 
+file_sha = file.sha
 
-print(file_content)
-
+repo.update_file(file_path, "FileUpdated", file_content, file_sha, branch="developer")
 
 
 
