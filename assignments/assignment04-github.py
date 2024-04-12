7808067372851b0b29471ca7eb60165111f03b72
Darach Gorham <darachg22@gmail.com>
2024-04-12 10:45:32 +0100

    Trying to resolve issues in pushing. Assignment 4 completed.

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -6,25 +6,23 @@
 #repo = g.get_repo("darachg22/WSAA-coursework")
 #content = repo.get_contents("assignments/Andrew.txt")
 #repo.update_file(content.path, "more tests", "more tests", content.sha, branch="test")
+
 from github import Github
-from config import config
 
-# Connect to GitHub using the token from config.py
-github_token = config["github_token"]
-g = Github(github_token)
-
-# Get the repository
+#Connect to GitHub
+g = Github("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")
 repo = g.get_repo("darachg22/WSAA-coursework")
 
-# Get the file contents
+#source code used: https://github.com/santiagomoneta/pygithub-example/blob/main/main.py
+#The first line here gets the text from the path given, using the branch as reference.
 file = repo.get_contents("assignments/Andrew.txt", ref="main")
+#This line create the file path to use again. 
 file_path = file.path
-
-# Modify the file content
-file_content = (file.decoded_content).decode('utf-8').replace("Andrew", "Darach")
+#The following line reads the file then converts it before replacing the words 'Andrew' with 'Darach'.
+file_content = (file.decoded_content).decode('utf-8').replace("Andrew", "Darach") 
+#This line retrieves something called the Secure Hash Algorithim, that identifies the content of the text file and verifies the changes. 
 file_sha = file.sha
-
-# Update the file
+#Here the text file gets updated with the new changes. 
 repo.update_file(file_path, "FileUpdated", file_content, file_sha, branch="main")
 
 
