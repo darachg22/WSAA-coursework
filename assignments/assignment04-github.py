81b870114f22665ae2eb6061610b075d17da9ba1
Darach Gorham <darachg22@gmail.com>
2024-04-12 10:03:58 +0100

    finsihed asssignment 4.

--- assignments/assignment04-github.py
+++ assignments/assignment04-github.py
@@ -1,10 +1,3 @@
-#Write a program in python that will read a file from a repository, 
-#The program should then replace all the instances of the text "Andrew" with your name. 
-#The program should then commit those changes and push the file back to the repository (You will need authorisation to do this).
-#I do not need to see your keys (see lab2, to follow)
-#Handup: Push the program as assignment04-github.py to assignments repository.
-#Marks: Marks will be given for the functionality and layout of the code; I do expect minimal comments to indicate you know what the program is doing.
-#token: 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
 #Source to read file: https://python-forum.io/thread-26072.html
 
 #from github import Github
@@ -21,12 +14,16 @@ g = Github("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")
 repo = g.get_repo("darachg22/WSAA-coursework")
 
 #source code used: https://github.com/santiagomoneta/pygithub-example/blob/main/main.py
-file = repo.get_contents("Assignments/Andrew.txt", ref="main")
+#The first line here gets the text from the path given, using the branch as reference.
+file = repo.get_contents("assignments/Andrew.txt", ref="main")
+#This line create the file path to use again. 
 file_path = file.path
+#The following line reads the file then converts it before replacing the words 'Andrew' with 'Darach'.
 file_content = (file.decoded_content).decode('utf-8').replace("Andrew", "Darach") 
+
 file_sha = file.sha
 
-repo.update_file(file_path, "FileUpdated", file_content, file_sha, branch="developer")
+repo.update_file(file_path, "FileUpdated", file_content, file_sha, branch="main")
 
 
 
